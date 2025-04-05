6d649e8d29127da0920a90b1120557224b6c9c96
Yagyesh Goyal <165410794+yagyeshgoyal@users.noreply.github.com>
2025-04-05 22:44:54 +0530

    Time: 8 ms (5.19%), Space: 27.5 MB (8.25%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -3,7 +3,6 @@ public:
     int trap(vector<int>& height) {
         
         vector<int>maxiwall(height.size());
-        vector<int>maxiwall2(height.size());
         stack<int>st;
         int r = -1;
         for(int i=height.size()-1; i>=0; i--){
@@ -27,46 +26,19 @@ public:
             st.push(i);
         }
 
-        while(st.size() != 0){
-            st.pop();
-        }
-
-        for(int i=0; i<height.size(); i++){
-            while(!st.empty()){
-                if(height[i]>height[st.top()]){
-                    st.pop();
-                }
-                else{
-                    break;
-                }
-            }
-
-            if(st.size() == 0){
-                maxiwall2[i] = -1;
-            }
-            else{
-                maxiwall2[i] = st.top();
-            }
-
-            st.push(i);
-        }
+       
 
         int ans = 0;
 
         int i = 0;
         while(i<height.size()){
             int k = maxiwall[i];
-            int p = maxiwall2[i];
+            
 
             if(k==-1){
                 i++;
             }
-            // else if(i>p && i<k && p != -1 && height[i] != height[p]){
-            //     for(int j=i; j<k; j++){
-            //         ans = ans + (height[k]-height[j]);
-            //     }
-            //     i = k;
-            // }
+            
             else if(height[i] > height[k]){
                 for(int j=i+1; j<k; j++){
                     ans = ans + (height[k]-height[j]);
