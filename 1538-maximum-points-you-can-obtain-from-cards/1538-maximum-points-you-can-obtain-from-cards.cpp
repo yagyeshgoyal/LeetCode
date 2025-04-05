ec9f5590035bc185e366f2d495d9cc794b86b517
Yagyesh Goyal <165410794+yagyeshgoyal@users.noreply.github.com>
2025-04-05 17:08:19 +0530

    Time: 2 ms (19.2%), Space: 50 MB (5.52%) - LeetHub

--- 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/1538-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,56 +1,6 @@
 class Solution {
-    int findans(int s, int e , int n, vector<int>&v, int k,vector<vector<int>>&dp){
-        if(n-e-1 + s == k){
-            return 0;
-        }
-
-        if(dp[s][e] != -1){
-            return dp[s][e];
-        }
-
-        int ans1 = v[s] + findans(s+1,e,n,v,k,dp);
-        int ans2 = v[e] + findans(s,e-1,n,v,k,dp);
-
-        return dp[s][e] = max(ans1,ans2);
-    }
-    int findans(int s, int e , int n, vector<int>&v, int k,map<string,int>&dp){
-        if(n-e-1 + s == k){
-            return 0;
-        }
-
-        string temp = to_string(s) +"#"+to_string(e);
-
-        if(dp.count(temp)){
-            return dp[temp];
-        }
-
-        int ans1 = v[s] + findans(s+1,e,n,v,k,dp);
-        int ans2 = v[e] + findans(s,e-1,n,v,k,dp);
-
-        return dp[temp] = max(ans1,ans2);
-    }
-    int findans(int s, int e , int n, vector<int>&v, int k,unordered_map<int,unordered_map<int,int>>&dp){
-        if(n-e-1 + s == k){
-            return 0;
-        }
-
-
-        if(dp.count(s) && dp[s].count(e)){
-            return dp[s][e];
-        }
-
-        int ans1 = v[s] + findans(s+1,e,n,v,k,dp);
-        int ans2 = v[e] + findans(s,e-1,n,v,k,dp);
-
-        return dp[s][e] = max(ans1,ans2);
-    }
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        
-        // vector<vector<int>>dp(cardPoints.size(), vector<int>(cardPoints.size(),-1));
-        // unordered_map<int,unordered_map<int,int>>dp;
-        // map<string,int>dp;
-        // return findans(0,cardPoints.size()-1,cardPoints.size(), cardPoints,k,dp);
 
         int n = cardPoints.size();
         vector<int>prefix(n+1,0);
