f9813a92895dcf27812e40bc6d58088bf9259cca
Yagyesh Goyal <165410794+yagyeshgoyal@users.noreply.github.com>
2025-04-17 18:33:35 +0530

    Time: 3 ms (67.89%), Space: 17.3 MB (49.54%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -27,14 +27,35 @@ public:
             mp[i]++;
         }
         vector<int>v;
+        v.push_back(-1);
         for(auto i : mp){
             v.push_back(i.first);
         }
 
-        sort(v.begin(),v.end());
-        vector<int>dp(v.size()+1, -1);
-        int ans = findans(0,v.size(),v,mp,dp);
+        // sort(v.begin(),v.end());
+        // vector<int>dp(v.size()+1, -1);
+        // int ans = findans(0,v.size(),v,mp,dp);
+        // return ans;
 
-        return ans;
+
+        vector<int>dp(v.size(),0);
+        dp[1] = v[1]*mp[v[1]];
+
+        for(int i=2; i<v.size(); i++){
+            if(v[i] == v[i-1]+1){
+                dp[i] = v[i]*mp[v[i]] ;
+                if(i-3>=0){
+                    dp[i] = dp[i] + max(dp[i-2],dp[i-3]);
+                }
+                else{
+                    dp[i] = dp[i] + dp[i-2];
+                }
+            }
+            else{
+                dp[i] = v[i]*mp[v[i]] + max(dp[i-1],dp[i-2]);
+            }
+        }
+
+        return max(dp[v.size()-1], dp[v.size()-2]);
     }
 };
