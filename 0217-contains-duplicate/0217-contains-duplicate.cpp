442f14d988da844cad56df1abd38ed080ade8684
Yagyesh Goyal <165410794+yagyeshgoyal@users.noreply.github.com>
2025-05-19 10:18:14 +0530

    Time: 20 ms (89.45%), Space: 70.8 MB (85.29%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        for(int i=0; i+1<nums.size(); i++){
+            if(nums[i] == nums[i+1]){
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
