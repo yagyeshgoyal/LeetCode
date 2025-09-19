42ac3f51cfa8dd856ebaa8e785c494908d60f4f2
Yagyesh Goyal <165410794+yagyeshgoyal@users.noreply.github.com>
2025-09-19 16:44:24 +0530

    Time: 332 ms (90.48%), Space: 0B (100%) - LeetHub

---
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/1136-actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,8 @@
+select actor_id, director_id 
+from(
+select actor_id,director_id, 
+count(timestamp) as cooperated 
+from ActorDirector 
+group by actor_id,director_id) 
+table1
+where cooperated>=3;
