bc0b8070ccd121b3d116421a4d8de114f941337d
Yagyesh Goyal <165410794+yagyeshgoyal@users.noreply.github.com>
2026-01-31 16:23:59 +0530

    Time: 0 ms (100%), Space: 9.4 MB (28.34%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -18,10 +18,28 @@ class Solution {
 
         return dp[i][j] = ans1+ans2;
     }
+
+    int findans1(int &m, int &n){
+        vector<vector<int>>dp(m+1,vector<int>(n+1,0));
+
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                dp[i][j] = dp[i+1][j] + dp[i][j+1];
+
+                if(i == m-1 && j==n-1){
+                    dp[i][j] = 1;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        int ans = findans(0,0,m,n,dp);
+        // int ans = findans(0,0,m,n,dp);
+
+        int ans = findans1(m, n);
 
         return ans;
     }
